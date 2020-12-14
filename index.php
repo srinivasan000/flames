db40564aed87ac112e2ae08a40c7ff448bb28b96
Srinivasan <66051429+srinivasan000@users.noreply.github.com>
2020-12-15 03:37:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,16 @@
 </head>
 <body style="text-align:center;background:linear-gradient(to right,#ff0000,#ff80f2">
     <h1>flames game</h1>
+<audio id="Srini">
+<source src="audio.mp3" type="audio/mpeg>
+</audio>
     <form action="index.php"method="POST">
     <input type="text" name="name1"id="name1"placeholder="enter name 1 "><br><br><br>
     <input type="text" name="name2"id="name2"placeholder="enter name 2 "><br><br><br>
     <input type="submit" name="flames" id="click" value="text">
     </form>
     <script>
+var audio=document.getElementById("Srini");
 var name1=document.getElementById("name1").value;
 var name2=document.getElementById("name2").value;
 document.getElementById('click').onclick = click;
@@ -46,6 +50,7 @@ function click() {
     }
 };
 function flip(coin) {
+   audio.play();
    alert("🙂"+coin+"🙂");
 };
 </script>
